7ea397f1590d11f399eb2b581f8e2aacc3dc4aee
Ayman Eltemsahi <aymaneltemsahi@hotmail.com>
2019-06-18 16:50:11 +0200

    publish

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+
+gulp.task('copy', function () {
+    return gulp.src('publish/index.html')
+        .pipe(gulp.dest('.'))
+        .pipe(rename('404.html'))
+});
